ea7b62278cd7fa87e84e44a3012ce7f8da8150a3
Greg Reinbach <greg@reinbach.com>
2012-07-01 19:35:17 -0400

    removed psycopg2 requirement

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 requires = [
     'Django==1.4',
-    'psycopg2==2.4.5',
+#    'psycopg2==2.4.5',
     'Jinja2==2.6',
     'Coffin==0.3.6',
     'Fabric==1.4.2',
@@ -15,6 +15,6 @@ setup(
     description='seahobo description',
     author='Greg Reinbach',
     author_email='greg@reinbach.com',
-    url='http://github.com/seahobo',
+    url='https://github.com/reinbach/seahobo',
     install_requires=requires,
 )
